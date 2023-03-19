22f276c16f10136c7a740d3cb8d845a550e8f24c
Jordan Petridis <jordan@centricular.com>
2023-03-20 01:09:56 +0200

    test-msvc: Install gtk into destdir and extract it into CI artifacts

--- .gitlab-ci/test-msvc.bat
+++ .gitlab-ci/test-msvc.bat
@@ -6,8 +6,10 @@ call "C:\Program Files (x86)\Microsoft Visual Studio\2017\BuildTools\VC\Auxiliar
 
 :: FIXME: make warnings fatal
 pip3 install --upgrade --user meson~=0.64 || goto :error
-meson setup -Ddebug=false -Dmedia-gstreamer=disabled _build || goto :error
+:: FIXME: we should not need fontconfig=disabled
+meson setup --prefix="C:\gtk4" -Ddebug=false -Dmedia-gstreamer=disabled _build -Dcairo:fontconfig=disabled -Dpango:fontconfig=disabled || goto :error
 meson compile -C _build || goto :error
+meson install --destdir=%CI_PROJECT_DIR%\destdir -C _build || goto :error
 
 goto :EOF
 :error
