25ac655f1d0e345ee1cd572ca3e9c6ae56e7114f
Gaël Bonithon <gael@xfce.org>
2023-12-10 10:17:00 +0100

    gtkdnd: Keep a reference on destination drag context
    It is unreffed in gtk_drag_source_info_destroy() and, if a reference is
    not kept, it can be released before we are finished with it, for example
    in gtk_drag_abort_timeout(). This can cause a crash when the dnd takes
    place in the GtkSocket/GtkPlug framework.

--- gtk/gtkdnd.c
+++ gtk/gtkdnd.c
@@ -1393,7 +1393,7 @@ gtk_drag_get_dest_info (GdkDragContext *context,
   if (!info && create)
     {
       info = g_slice_new0 (GtkDragDestInfo);
-      info->context = context;
+      info->context = g_object_ref (context);
       g_object_set_qdata_full (G_OBJECT (context), info_quark,
                                info, gtk_drag_dest_info_destroy);
     }
